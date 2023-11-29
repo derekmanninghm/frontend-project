bebac2d52129985c0aca566a11bb385add628bcb
derek_manning <derekmanninghm@gmail.com>
2023-11-29 15:12:07 -0800

    working

--- index.js
+++ index.js
@@ -12,33 +12,27 @@ const options = {
 //console.log(options);
 
 
-$.get("https://api.dicebear.com/7.x/open-peeps/svg?seed=John&size=300&flip=true", (d)=> {
-    
-    $("#currChar").prepend(d.firstChild);
-
-})
-
 
 var skinColorArr = ['694d3d', 'ae5d29', 'd08b5b', 'ffdbb4'];
 
-var accessoriesArr = [null, 'eyepatch', 'glasses', 'glasses2', 'glasses3', 'glasses4', 'glasses5', 'sunglasses', 'sunglasses2'];
-var clothingColorsArr = [null, '8fa7df', '9ddadb', '78e185', 'e279c7', 'e78276', 'fdea6b', 'ffcf77'];
+var accessoriesArr = ['none', 'eyepatch', 'glasses', 'glasses2', 'glasses3', 'glasses4', 'glasses5', 'sunglasses', 'sunglasses2'];
+var clothingColorsArr = ['8fa7df', '9ddadb', '78e185', 'e279c7', 'e78276', 'fdea6b', 'ffcf77'];
 var facesArr = 
     [ 
-        null, 'angryWithFang', 'awe', 'blank', 'calm', 'cheeky','concerned', 'concernedFear', 'contempt', 
+        'angryWithFang', 'awe', 'blank', 'calm', 'cheeky','concerned', 'concernedFear', 'contempt', 
         'cute', 'cyclops', 'driven', 'eatingHappy', 'explaining', 'eyesClosed', 'fear', 'hectic', 'lovingGrin1',
         'lovingGrin2', 'monster', 'old', 'rage','serious','smile','smileBig','smileLOL','smileTeethGap', 
         'solemn', 'suspicious', 'tired', 'veryAngry'
     ];
 var facialHairsArr = 
     [ 
-        null, 'chin', 'full', 'full2', 'full3', 'full4','goatee1', 'goatee2', 'moustache1', 'moustache2',
+        'none', 'chin', 'full', 'full2', 'full3', 'full4','goatee1', 'goatee2', 'moustache1', 'moustache2',
         'moustache3', 'moustache4', 'moustache5', 'moustache7', 'moustache8', 'moustache9', 
     ];
     
     var headsArr =
     [ 
-        null, 'afro', 'bangs', 'bangs2', 'bantuKnots', 'bear','bun', 'bun2', 'buns', 'cornrows', 'cornrows2', 'dreads1', 'dreads2', 
+        'afro', 'bangs', 'bangs2', 'bantuKnots', 'bear','bun', 'bun2', 'buns', 'cornrows', 'cornrows2', 'dreads1', 'dreads2', 
         'flatTop', 'flatTopLong', 'grayMedium', 'grayShort', 'hatBeanie', 'hatHip', 'hijab', 'long','longAfro','longBangs',
         'longCurly','medium1','medium2', 'medium3', 'mediumBangs', 'mediumBangs2', 'mediumBangs3', 'mediumStraight', 'mohawk',
         'noHair1', 'noHair3', 'pomp', 'shaved1', 'shaved2', 'shaved3', 'short1', 'short2', 'short3', 'short4',
@@ -48,67 +42,149 @@ var facialHairsArr =
 const currentCharacterOptions = {
     seed: "John",
     size: '300',
-    skinColor: 'edb98a',
-    accessories: 'eyepatch',
-    accessoriesProbability: `100`,
+    skinColor: 'none',
+    accessories: accessoriesArr,
+    accessoriesProbability: `85`,
     clothingColor: '8fa7df',
     head: 'hatBeanie',
-    face: 'driven',
-    facialHair: null,
-    facialHairrobability: null,
+    face: 'suspicious',
+    facialHair: facialHairsArr,
+    facialHairProbability: '85',
 }
 
-function generateApiURL() {
-    var currentApiUrl = `https://api.dicebear.com/7.x/open-peeps/svg?`
-    (currentCharacterOptions.accessories != null)
-    return currentApiUrl;
+$("#randomize").on("click", (e)=> {
+    console.log(currentCharacterOptions)
+    generateCurrCharIcon();
+})
+
+
+function generateCurrCharIcon() {
+    $("#currCharIcon").empty();
+    //note - to incorporate more random features
+    var apiURL = `https://api.dicebear.com/7.x/open-peeps/svg?`;
+    apiURL += `seed=${currentCharacterOptions.seed}`
+    apiURL += `&size=${currentCharacterOptions.size}`
+    if(currentCharacterOptions.skinColor !== 'none') apiURL += `&skinColor=${currentCharacterOptions.skinColor}`;
+    
+    if(currentCharacterOptions.accessories !== 'none') {
+        if(currentCharacterOptions.accessories == accessoriesArr) {
+            apiURL += `&accessories=${currentCharacterOptions.accessories[Math.floor(Math.random() * (7 - 1) + 1)]}`;
+            apiURL += `&accessoriesProbability=${currentCharacterOptions.accessoriesProbability}`;
+        } else {
+            apiURL += `&accessories=${currentCharacterOptions.accessories}`;
+            apiURL += `&accessoriesProbability=100`;
+        }
+    }
+    
+    if(currentCharacterOptions.clothingColor !== 'none')    apiURL += `&clothingColor=${currentCharacterOptions.clothingColor}`
+    if(currentCharacterOptions.head !== 'none')apiURL += `&head=${currentCharacterOptions.head}`
+    if(currentCharacterOptions.face !== 'none')apiURL += `&face=${currentCharacterOptions.face}`
+    
+    if(currentCharacterOptions.facialHair !== 'none') {
+        if(currentCharacterOptions.facialHair == facialHairsArr) {
+            apiURL += `&facialHair=${currentCharacterOptions.facialHair[Math.floor(Math.random() * 15)]}`;
+            apiURL += `&facialHairProbability=${currentCharacterOptions.facialHairProbability}`;
+        } else {
+            apiURL += `&facialHair=${currentCharacterOptions.facialHair}`;
+            apiURL += `&facialHairProbability=100`;
+        }
+    }
+
+    console.log(apiURL)
+
+    $.get(`${apiURL+"&flip=true"}`, (d)=> {
+    
+        $("#currCharIcon").prepend(d.firstChild);
+
+    })
 }
 
-function displayAccessoryOptions() {
-        for(var i= 0; i<$("#accessoryContainer").get(0).children.length; i++) {
+generateCurrCharIcon()
 
-            var currentAccessory = $("#accessoryContainer").get(0).children[i].id
-            var currentAccessoryString;
-            currentAccessoryString =  `accessories=${currentAccessory}&accessoriesProbability=100`;
-            $(`#${$("#accessoryContainer").get(0).children[i].id}`).css("background-image", `url("https://api.dicebear.com/7.x/open-peeps/svg?${currentAccessoryString}")`)
+function generateFaceOptions() {
+    for(var i = 0; i < facesArr.length; i++) {
+        $("#faceContainer").append($("<button>").attr("id", facesArr[i]).attr("class", "iconOption").text(facesArr[i]));
         
-        }     
-}    
-    
-function generateCharOptions() {
-    displayAccessoryOptions();
+        $(`#${facesArr[i]}`).on("click", (e)=> {
+            currentCharacterOptions.face = (e.target.id)
+            generateCurrCharIcon();
+        })
+    }
 }
+
+function generateFacialHairOptions() {
+    for(var i = 0; i < facialHairsArr.length; i++) {
+        $("#faceContainer").append($("<button>").attr("id", facialHairsArr[i]).attr("class", "iconOption").text(facialHairsArr[i]));
+        
+        $(`#${facialHairsArr[i]}`).on("click", (e)=> {
+            currentCharacterOptions.facialHair = (e.target.id);
+            currentCharacterOptions.facialHairProbability = '100';
+            generateCurrCharIcon();
+        })
+    }
+}
+
+function generateHeadOptions() {
+    for(var i = 0; i < headsArr.length; i++) {
+        $("#headContainer").append($("<button>").attr("id", headsArr[i]).attr("class", "iconOption").text(headsArr[i]));
+        
+        $(`#${headsArr[i]}`).on("click", (e)=> {
+            currentCharacterOptions.head = (e.target.id)
+            generateCurrCharIcon();
+        })
+    }
+}
+
+function generateSkinOptions() {
+    for(var i = 0; i < skinColorArr.length; i++) {
+        $("#skinColorContainer").append($("<button>").attr("id", skinColorArr[i]).attr("class", "iconOption").text(skinColorArr[i]));
+        
+        $(`#${skinColorArr[i]}`).on("click", (e)=> {
+            currentCharacterOptions.skinColor = (e.target.id)
+            generateCurrCharIcon();
+        })
+    }
+}
+
+function generateClothingOptions() {
+    for(var i = 0; i < clothingColorsArr.length; i++) {
+        $("#clothsContainer").append($("<button>").attr("id", clothingColorsArr[i]).attr("class", "iconOption").text(clothingColorsArr[i]));
+        
+        $(`#${clothingColorsArr[i]}`).on("click", (e)=> {
+            currentCharacterOptions.clothingColor = (e.target.id)
+            generateCurrCharIcon();
+        })
+    }
+}
+
+function generateAccessoryOptions() {
+    for(var i = 0; i < accessoriesArr.length; i++) {
+        $("#accessoryContainer").append($("<button>").attr("id", accessoriesArr[i]).attr("class", "iconOption").text(accessoriesArr[i]));
     
-generateCharOptions();
-    //var seed = getSeed();
-    //var size = getSize();
-    //var skinColor = getSkinColor();
-    //var accessories = getAccessories();
-    //var accessoriesProbability= getAccessoriesProbability();
-    //var clothingColor = getClothingColor();
-    //var head = getHead();
-    //var face = getFace();
-    //var facialHair = getFacialHair();
-    //var facialHairProbability = getfacialHairProbability();
-    
-/*
+        $(`#${accessoriesArr[i]}`).on("click", (e)=> {
+            currentCharacterOptions.accessories = (e.target.id)
+            generateCurrCharIcon();
+        })
+    }
+}
 
-displayAccessoryOptions()
-*/
-
-
-
+generateFacialHairOptions()
+generateHeadOptions()
+generateFaceOptions()
+generateSkinOptions()
+generateAccessoryOptions()
+generateClothingOptions()
 
 
 var Maze, MazeGame;
 
-  const makeMaze = (id, width, height, speech = false) => {
+const makeMaze = (id, width, height, speech = false) => {
     Maze = new FancyMazeBuilder(width, height);
     Maze.display(id);
     MazeGame = new Mazing("maze");
     if(speech) {
-      MazeGame.enableSpeech();
+        MazeGame.enableSpeech();
     }
   };
-
+  
   makeMaze("maze_container", 12, 12);
